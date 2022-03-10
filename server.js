01b265b4f541154af2dc8f2a36d835009e7324ef
STEVIEMILITELLO <steviemilitello@gmail.com>
2022-03-10 16:09:38 -0500

    moved middleware to utils

--- server.js
+++ server.js
@@ -5,17 +5,13 @@
 // this allows us to load our env variables
 require('dotenv').config()
 const express = require('express')
-const morgan = require('morgan')
-const methodOverride = require('method-override')
 // We no longer need this reference because it lives in the fruit controller now
 // const Fruit = require('./models/fruit')
 // now that we're using controllers as they should be used
 // we need to require our routers
 const FruitRouter = require('./controllers/fruit')
 const UserRouter = require('./controllers/user')
-// session middleware requirements
-const session = require('express-session')
-const MongoStore = require('connect-mongo')
+const middleware = require('./utils/middleware')
 
 const app = require('liquid-express-views')(express())
 
@@ -23,22 +19,7 @@ const app = require('liquid-express-views')(express())
 ////////////// MIDDLEWARE ///////////////////////
 /////////////////////////////////////////////////
 
-// this is for request logging
-app.use(morgan('tiny'))
-app.use(methodOverride('_method'))
-// parses urlencoded request bodies
-app.use(express.urlencoded({ extended: false }))
-// to serve files from public statically
-app.use(express.static('public'))
-// this is the middleware to set up a session
-app.use(
-    session({
-        secret: process.env.SECRET,  
-        store: MongoStore.create({mongoUrl: process.env.DATABASE_URL}),
-        saveUninitialized: true,
-        resave: false
-    })
-)
+middleware(app)
 
 /////////////////////////////////////////////////
 ////////////// ROUTES ///////////////////////////
